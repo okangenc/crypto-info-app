c9c3c83d9bc79691047090074f360066c7b38886
okangenc <okigenc1234@gmail.com>
2023-08-11 11:31:50 +0100

    News page

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -13,13 +13,13 @@ const createRequest = (url) => ({ url, headers: cryptoNewsHeaders})
 
 
 export const cryptoNewsApi = createApi({
-    reducerPath: 'cryptoNewsApi',
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-        getCryptoNews: builder.query({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
         query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
-        }),
     }),
-    });
+  }),
+});
 
-    export const { useGetCryptoNewsQuery } = cryptoNewsApi;
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
