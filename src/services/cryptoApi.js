e38d76b1426691e73e1c55357aeac14a3e124a6b
okangenc <okigenc1234@gmail.com>
2023-08-13 14:51:47 +0100

    Working to fix details page

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const cryptoApiHeaders = {
     'X-RapidAPI-Key': 'c8c536d75fmshfc231356098d6c5p1f2a20jsn914e227cdc75',
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-}
+  }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
@@ -13,12 +13,19 @@ export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
+
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`),
-        })
+          }),
+      
+          getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+          }),
+        
     })
 });
 
 export const {
     useGetCryptosQuery,
+    useGetCryptoDetailsQuery,
 } = cryptoApi;
